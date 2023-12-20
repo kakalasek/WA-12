6cb9cc92e5e91e8f0c501529b026cc8de7183e81
kakalasek <honziccekkj@seznam.cz>
2023-12-20 09:05:18 +0100

    finish, but still not working

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const canvas = document.getElementById("cnv");
 const ctx = canvas.getContext("2d");
 let coord = { x: 0, y: 0 };
 
-function draw() {
+function draw(event) {
   ctx.beginPath();
   ctx.lineWidth = document.getElementById("stroke-width").value;
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#ACD3ED";
+  ctx.strokeStyle = document.getElementById("color-type").value;
   ctx.moveTo(coord.x, coord.y);
   reposition(event);
   ctx.lineTo(coord.x, coord.y);
@@ -27,5 +27,39 @@ function reposition(event) {
   coord.y = event.clientY - canvas.offsetTop;
 }
 
+clearAll = () => {
+  ctx.save();
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.restore();
+};
+
+importImage = () => {
+  let img = new Image();
+  img.src = document.getElementById("img-url").value;
+  ctx.drawImage(img, 0, 0);
+};
+
+function resizeCanvasToDisplaySize() {
+  const width = ctx.canvas.clientWidth;
+  const height = ctx.canvas.clientHeight;
+
+  if (ctx.canvas.width !== width || ctx.canvas.height !== height) {
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    return true;
+  }
+
+  return false;
+}
+
+resizeCanvasToDisplaySize();
+
 document.addEventListener("mousedown", start);
 document.addEventListener("mouseup", stop);
+document.getElementById("clear-all").addEventListener("click", clearAll);
+document
+  .getElementById("img-url-button")
+  .addEventListener("click", importImage);
